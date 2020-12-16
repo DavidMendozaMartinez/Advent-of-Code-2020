b79affcfb1580490ce33bce68aa2017bf043e5ec
David Mendoza <david.mendoza.mnez@gmail.com>
2020-12-16 22:23:04 +0100

    Refactor Input class

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -2,32 +2,33 @@ import java.io.File
 
 class Input(day: String) {
     private val file = File("src/main/kotlin/day_$day/input.txt")
+    private val text: String = file.readText()
+    private val lines: List<String> = file.readLines()
 
-    fun getStrings(block: String.() -> String = { this }): List<String> = file.readLines().map(block)
+    fun getStrings(block: String.() -> String = { this }): List<String> = lines.map(block)
 
-    fun getInts(): List<Int> = file.readLines().map { it.toInt() }
+    fun getInts(): List<Int> = lines.map { it.toInt() }
 
-    fun getLongs(): List<Long> = file.readLines().map { it.toLong() }
+    fun getLongs(): List<Long> = lines.map { it.toLong() }
 
-    fun getSplitStrings(vararg delimiters: Char): List<String> = file.readText().split(*delimiters)
+    fun getSplitStrings(vararg delimiters: Char): List<String> = text.split(*delimiters)
 
-    fun getSplitInts(vararg delimiters: Char): List<Int> = file.readText().split(*delimiters).map { it.toInt() }
+    fun getSplitInts(vararg delimiters: Char): List<Int> = text.split(*delimiters).map { it.toInt() }
 
     fun getStringPairs(delimiter: String): List<Pair<String, String>> =
-        file.readLines().map { it.substringBefore(delimiter) to it.substringAfter(delimiter) }
+        lines.map { it.substringBefore(delimiter) to it.substringAfter(delimiter) }
 
-    fun getCharMatrix(): List<List<Char>> = file.readLines().map { it.toList() }
+    fun getCharMatrix(): List<List<Char>> = lines.map { it.toList() }
 
     fun getStringGroupList(elementDelimiter: String, stringDelimiter: String): List<List<String>> =
-        file.readText().split(elementDelimiter).map { it.split(stringDelimiter) }
+        text.split(elementDelimiter).map { it.split(stringDelimiter) }
 
     fun getStringMapList(
         elementDelimiter: String,
         vararg pairDelimiter: String,
         stringDelimiter: String
     ): List<Map<String, String>> =
-        file.readText()
-            .split(elementDelimiter)
+        text.split(elementDelimiter)
             .map { element ->
                 element
                     .split(*pairDelimiter)
