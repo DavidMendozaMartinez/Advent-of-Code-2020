d206843f59f6c7140c5b4e77785120ba183d179b
David Mendoza <david.mendoza.mnez@gmail.com>
2020-12-19 19:59:59 +0100

    Add Day 12 solution

---
+++ src/main/kotlin/day_12/Day12.kt
@@ -0,0 +1,78 @@
+package day_12
+
+import Day
+import Input
+import kotlin.math.abs
+
+fun main() {
+    val dayNumber = "12"
+    val input = Input(dayNumber).getStrings()
+    Day12(input).solve(dayNumber)
+}
+
+class Day12(input: List<String>) : Day<Int, Int>() {
+    private val instructions: List<Instruction> = input.map { Instruction.parse(it) }
+
+    override fun solvePart1(): Int =
+        Navigation(facing = Direction.EAST, execution = ::interpret).apply { run(instructions) }.manhattanDistance()
+
+    override fun solvePart2(): Int =
+        Navigation(facing = Direction.EAST, waypoint = Position(10, 1)).apply { run(instructions) }.manhattanDistance()
+
+    private fun interpret(navigation: Navigation, instruction: Instruction): Unit = with(navigation) {
+        when (instruction.action) {
+            Action.N -> ship.y += instruction.value
+            Action.S -> ship.y -= instruction.value
+            Action.E -> ship.x += instruction.value
+            Action.W -> ship.x -= instruction.value
+            Action.L -> facing = facing.left(instruction.value / 90)
+            Action.R -> facing = facing.right(instruction.value / 90)
+            Action.F -> interpret(this, Instruction(Action.valueOf(facing.name.take(1)), instruction.value))
+        }
+    }
+
+    class Navigation(
+        var facing: Direction,
+        var ship: Position = Position(),
+        var waypoint: Position = Position(),
+        var execution: (Navigation, Instruction) -> Unit = Navigation::execute,
+    ) {
+        fun run(instructions: List<Instruction>) = instructions.forEach { execution(this, it) }
+
+        private fun execute(instruction: Instruction) {
+            when (instruction.action) {
+                Action.N -> waypoint.y += instruction.value
+                Action.S -> waypoint.y -= instruction.value
+                Action.E -> waypoint.x += instruction.value
+                Action.W -> waypoint.x -= instruction.value
+                Action.L -> repeat(instruction.value / 90) { waypoint = Position(-waypoint.y, waypoint.x) }
+                Action.R -> repeat(instruction.value / 90) { waypoint = Position(waypoint.y, -waypoint.x) }
+                Action.F -> ship.apply {
+                    x += waypoint.x * instruction.value
+                    y += waypoint.y * instruction.value
+                }
+            }
+        }
+
+        fun manhattanDistance(starting: Position = Position()): Int =
+            abs(ship.x - starting.x) + abs(ship.y - starting.y)
+    }
+
+    enum class Direction {
+        NORTH, EAST, SOUTH, WEST;
+
+        fun left(times: Int): Direction = values()[Math.floorMod(ordinal - times, values().size)]
+        fun right(times: Int): Direction = values()[Math.floorMod(ordinal + times, values().size)]
+    }
+
+    data class Position(var x: Int = 0, var y: Int = 0)
+
+    class Instruction(val action: Action, val value: Int) {
+        companion object {
+            fun parse(instructionStr: String): Instruction =
+                Instruction(Action.valueOf(instructionStr.take(1)), instructionStr.drop(1).toInt())
+        }
+    }
+
+    enum class Action { N, S, E, W, L, R, F }
+}
---
+++ src/test/kotlin/day_12/Day12Test.kt
@@ -0,0 +1,28 @@
+package day_12
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class Day12Test {
+
+    private lateinit var day: Day12
+
+    @Before
+    fun setUp() {
+        val input = listOf("F10", "N3", "F7", "R90", "F11")
+        day = Day12(input)
+    }
+
+    @Test
+    fun `Day 12 - Part 1 - Expected output when providing sample input`() {
+        val expected = 25
+        assertEquals(expected, day.solvePart1())
+    }
+
+    @Test
+    fun `Day 12 - Part 2 - Expected output when providing sample input`() {
+        val expected = 286
+        assertEquals(expected, day.solvePart2())
+    }
+}
