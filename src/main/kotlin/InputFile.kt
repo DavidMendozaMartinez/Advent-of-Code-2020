290fe989c866394f0b34bb54bfbffc83b0e54328
David Mendoza <david.mendoza.mnez@gmail.com>
2021-01-14 17:57:25 +0100

    Refactor the InputFile class

--- src/main/kotlin/InputFile.kt
+++ src/main/kotlin/InputFile.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-class InputFile(val dayNumber: String) {
+class InputFile(dayNumber: String) {
     private val file = File("src/main/kotlin/day_$dayNumber/input.txt")
 
     var text: String = file.readText()
